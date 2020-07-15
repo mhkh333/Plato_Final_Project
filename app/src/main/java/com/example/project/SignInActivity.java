d614f449c78f26f8cfef60518b3b7ecc73a863fa
Mohammad Hossein Khorrami <mohammadkhoram3@gmail.com>
2020-07-15 16:40:13 +0430

    initial commit

---
+++ app/src/main/java/com/example/project/SignInActivity.java
@@ -0,0 +1,27 @@
+package com.example.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignInActivity extends AppCompatActivity {
+    boolean isOkBoth = true;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin_activity);
+        Button button = this.findViewById(R.id.button2);
+        final Intent intent = new Intent(this,MainPageAsliActivity.class);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isOkBoth)
+                startActivity(intent);
+            }
+        });
+    }
+}
