1ba1bdc9ff1ffc5ed3cc466f202292b2fa79d932
Mohammad Hossein Khorrami <mohammadkhoram3@gmail.com>
2020-07-16 14:22:09 +0430

    check??

--- app/src/main/java/com/example/project/SignInActivity.java
+++ app/src/main/java/com/example/project/SignInActivity.java
@@ -24,7 +24,7 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin_activity);
-        System.out.println("Check?");
+
 
         name = findViewById(R.id.PersonName);
         password = findViewById(R.id.Password);
