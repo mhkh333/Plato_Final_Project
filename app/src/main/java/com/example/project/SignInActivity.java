9005931c386fcfee466b0030fa4751bcd920cd2a
Mohammad Hossein Khorrami <mohammadkhoram3@gmail.com>
2020-07-16 14:10:24 +0430

    tried for server

--- app/src/main/java/com/example/project/SignInActivity.java
+++ app/src/main/java/com/example/project/SignInActivity.java
@@ -4,25 +4,62 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class SignInActivity extends AppCompatActivity {
-    boolean isOkBoth = true;
+
+    private EditText name;
+    private EditText password;
+
+    boolean isOkBoth = false;
+    public ArrayList<String> haj = new ArrayList<>();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin_activity);
-        Button button = this.findViewById(R.id.button2);
-        final Intent intent = new Intent(this,MainPageAsliActivity.class);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        name = findViewById(R.id.PersonName);
+        password = findViewById(R.id.Password);
+        Button signIn = findViewById(R.id.button2);
+
+
+        final Intent intent = new Intent(this, MainPageAsliActivity.class);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isOkBoth)
-                startActivity(intent);
+                String inputName = name.getText().toString();
+                String inputPassword = password.getText().toString();
+
+                if (inputName.isEmpty() || inputPassword.isEmpty()) {
+                    Toast.makeText(SignInActivity.this, "please enter more than zero", Toast.LENGTH_LONG).show();
+                } else {
+                    isOkBoth = valid(inputName, inputPassword);
+
+                    if (!isOkBoth) {
+
+                        Toast.makeText(SignInActivity.this, "please enter correct", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(SignInActivity.this, "Login succeeded", Toast.LENGTH_LONG).show();
+                        startActivity(intent);
+                    }
+                }
             }
         });
-        System.out.println("Hi Ostad");
+
+    }
+
+    //if contains in arrayList must be here
+    private boolean valid(String name, String password) {
+        String use = "Khorram";
+        String pass = "0921";
+        return name.equals(use) && password.equals(pass);
     }
 }
--- app/src/main/java/com/example/project/SignUpActivity.java
+++ app/src/main/java/com/example/project/SignUpActivity.java
@@ -1,16 +1,73 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    boolean isOkName = false;
+    ArrayList<String> userNames = new ArrayList<>();
+    ArrayList<String> passwords = new ArrayList<>();
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        Button button = this.findViewById(R.id.)
+        DataOutputStream dos = null;
+        DataInputStream dis = null;
+        Socket socket = null;
+        try {
+            socket = new Socket("192.168.1.177", 3330);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            dos = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            dis = new DataInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final EditText name = findViewById(R.id.editTextTextPersonName);
+        final EditText passWord = findViewById(R.id.editTextTextPassword);
+        Button button = this.findViewById(R.id.button);
+
+        final DataOutputStream finalDos = dos;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    finalDos.writeUTF("signUp");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String input = name.getText().toString();
+                if (!userNames.contains(input)) {
+                    userNames.add(input);
+                    String inputPass = passWord.getText().toString();
+                    passwords.add(inputPass);
+                } else {
+                    Toast.makeText(SignUpActivity.this, "please enter another username we already have it :]", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
